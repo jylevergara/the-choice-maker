7b8a2e33142e83343c23f30220e33695a62c620d
Jyle Vergara <jylevergara@gmail.com>
2019-10-16 04:36:12 +0800

    add another choice button

--- OptionScreen.js
+++ OptionScreen.js
@@ -2,24 +2,43 @@ import React, { useState } from 'react';
 import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function OptionScreen(props) {
-  const [value, setValue] = useState('');
-  const [todo, setTodo] = useState('');
-  const choices = [];
+  const [values, setValues] = useState('');
+  const [choices, setChoices] = useState(['']);
 
-  function handleChangeText(text) {
-    setValue(text);
+  const handleChangeText = name => (event, value) => {
+    setValues({
+      ...values,
+      [name]: event.target.value,
+    });
+  };
+
+  function addAnotherChoice() {
+    // e.preventDefault();
+    let choice = choices.concat(['']);
+    setChoices(choice);
   }
+
+  const renderChoices = () => {
+    return choices.map((choice, i) => {
+      return (
+        <TextInput
+          key={i}
+          placeholder="Type your choice here..."
+          style={{ height: 40, borderColor: 'gray', margin: 8, borderWidth: 1 }}
+          onChangeText={handleChangeText(i)}
+          value={choice}
+        />
+      );
+    });
+  };
 
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="Type your choice here..."
-        style={{ height: 40, borderColor: 'gray', margin: 8, borderWidth: 1, flex: 1 }}
-        onChangeText={text => handleChangeText(text)}
-        value={value}
-      />
-      <Button title="Add Another Choice" />
+      <View>
+        {renderChoices()}
+        <Button title="Add Another Choice" onPress={addAnotherChoice} />
+      </View>
       <Button title="Decide" onPress={() => props.navigation.navigate('Decision')} />
     </View>
   );
@@ -28,9 +47,9 @@ export default function OptionScreen(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    justifyContent: 'space-around',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
